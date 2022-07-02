3b11e9323b0b297e071e29ac55598287f3fab3da
Kristian Quirapas <kmquirapas@gmail.com>
2022-07-02 19:15:37 +0800

    Removed helmet and snap

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
-import { Helmet } from 'react-helmet'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
@@ -15,40 +14,6 @@ import NotFound from './components/NotFound';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-		<Helmet>
-			<title>SkidsDAO</title>
-			<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
-
-      <meta property="og:title" content="SkidsDAO" />
-      <meta property="og:url" content="https://skidsdao.github.io" />
-      <meta property="og:description" content="Breaking your things since 2022." />
-      <meta property="og:image" content="/assets/skidsdao_logo.png" />
-      <meta property="og:type" content="website" /> 
-
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:title" content="SkidsDAO" />
-      <meta name="twitter:url" content="https://skidsdao.github.io" />
-      <meta name="twitter:description" content="Breaking your things since 2022." />
-      <meta name="twitter:image" content="/assets/skidsdao_logo.png" /> 
-
-			<link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png"/>
-			<link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png"/>
-			<link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png"/>
-			<link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png"/>
-			<link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png"/>
-			<link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png"/>
-			<link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png"/>
-			<link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png"/>
-			<link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png"/>
-			<link rel="icon" type="image/png" sizes="192x192"  href="/android-icon-192x192.png"/>
-			<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
-			<link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png"/>
-			<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
-			<link rel="manifest" href="/manifest.json"/>
-			<meta name="msapplication-TileColor" content="#ffffff"/>
-			<meta name="msapplication-TileImage" content="/ms-icon-144x144.png"/>
-			<meta name="theme-color" content="#ffffff"/>
-		</Helmet>
 		<BrowserRouter>
 			<Routes>
 				<Route exact path="/" element={<Home />} />
